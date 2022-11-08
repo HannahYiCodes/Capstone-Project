9395ced228d914205b75d90951a9e780310600aa
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-11-08 18:17:35 -0500

    Capstone

--- src/main/java/com/careerdevs/capstone/repositories/AuctionRepository.java
+++ src/main/java/com/careerdevs/capstone/repositories/AuctionRepository.java
@@ -1,4 +1,11 @@
 package com.careerdevs.capstone.repositories;
 
+import com.careerdevs.capstone.models.Auction;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
 public interface AuctionRepository {
+    // findBySerial
+    Optional<Auction> findBySerialId(Long serialNumber);
 }
